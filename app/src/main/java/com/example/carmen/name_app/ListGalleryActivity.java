39ff0044e1bd9f6d0b38862417040cf32add9a9c
CarmenJiang <cji96@hotmail.com>
2018-01-16 16:22:53 +0000

    Added functionality to home button

--- app/src/main/java/com/example/carmen/name_app/ListGalleryActivity.java
+++ app/src/main/java/com/example/carmen/name_app/ListGalleryActivity.java
@@ -1,5 +1,6 @@
 package com.example.carmen.name_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -19,12 +21,21 @@ public class ListGalleryActivity extends AppCompatActivity {
 
         GridView gridview = (GridView) findViewById(R.id.gridView);
         gridview.setAdapter(new ImageAdapter(this));
+        final Button buttonHome = findViewById(R.id.buttonHome);
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
                 Toast.makeText(ListGalleryActivity.this, "" + position,
                         Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ListGalleryActivity.this, MainActivity.class);
+                startActivity(i);
             }
         });
     }
