8a5f6af6f12f9ae651cdb89c99b7fdabfa6f4f6f
Markus <markus.loland@hotmail.com>
2018-01-16 17:23:45 +0100

    Add listActivity, settingsActivity, showPersonActivity

---
+++ app/src/main/java/com/example/carmen/name_app/ListActivity.java
@@ -0,0 +1,49 @@
+package com.example.eier.testingstuff;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        final String[] aArr = getResources().getStringArray(R.array.people);
+
+        final String[] bArr = new String[aArr.length];
+        for(int i = 0; i < aArr.length; i++){
+            bArr[i] = aArr[i].split("\\+")[0];
+
+
+        }
+
+        final ListView listView = (ListView) findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, bArr);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(ListActivity.this, showPersonActivity.class);
+                String[] sArr = getResources().getStringArray(R.array.people)[i].split("|");
+                intent.putExtra("name", aArr[i]);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/carmen/name_app/showPersonActivity.java
@@ -0,0 +1,48 @@
+package com.example.eier.testingstuff;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.net.URI;
+
+public class showPersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_person);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView textView = (TextView) findViewById(R.id.personName);
+        ImageView imageView = (ImageView) findViewById(R.id.personPicture);
+        String[] info = getIntent().getStringExtra("name").split("\\+");
+        textView.setText(info[0]);
+
+
+        //set Picture
+        Context context = imageView.getContext();
+        int id = context.getResources().getIdentifier(info[1], "drawable", context.getPackageName());
+        imageView.setImageResource(id);
+
+
+
+
+
+
+       /* TextView textView = (TextView) findViewById(R.id.personName);*/
+        setSupportActionBar(toolbar);
+
+
+    }
+
+
+
+}
