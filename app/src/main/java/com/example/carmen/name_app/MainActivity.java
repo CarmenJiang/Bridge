22f334a14f881f57ba2c73d7d8cc0e104e1de27c
Markus <markus.loland@hotmail.com>
2018-01-17 11:17:08 +0100

    Added the rest of the layouts and activities, and deleted not used ones

---
+++ app/src/main/java/com/example/carmen/name_app/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.eier.testingstuff;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton settingsbtn = (ImageButton)findViewById(R.id.settingsButton);
+        if(settingsbtn == null)
+            Log.i("adw","eoiadjoi");
+        settingsbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
+
+        Button listbtn = (Button)findViewById(R.id.listButton);
+
+        listbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ListActivity.class));
+            }
+        });
+    }
+}
