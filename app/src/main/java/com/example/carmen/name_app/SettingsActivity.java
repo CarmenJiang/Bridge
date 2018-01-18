f70879655345c7e92f049a62c8bb050358c7e073
CarmenJiang <cji96@hotmail.com>
2018-01-18 11:48:52 +0000

    Added functionalyti to Home button

--- app/src/main/java/com/example/carmen/name_app/SettingsActivity.java
+++ app/src/main/java/com/example/carmen/name_app/SettingsActivity.java
@@ -1,11 +1,13 @@
 package com.example.carmen.name_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -13,8 +15,16 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        final Button button = findViewById(R.id.buttonHome);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
 
 
     }
