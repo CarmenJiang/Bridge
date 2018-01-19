c40b59531c278e5123fc30557ab4211690cf667e
Markus <markus.loland@hotmail.com>
2018-01-19 13:09:59 +0100

    viewScore, and music

--- app/src/main/java/com/example/carmen/name_app/ListActivity.java
+++ app/src/main/java/com/example/carmen/name_app/ListActivity.java
@@ -1,6 +1,7 @@
 package com.example.carmen.name_app;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,10 +18,18 @@ import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bensound_jazzyfrenchy);
+
+            mediaPlayer.start();
+
+
         final Button button = findViewById(R.id.buttonHome);
 
         button.setOnClickListener(new View.OnClickListener(){
@@ -51,6 +60,17 @@ public class ListActivity extends AppCompatActivity {
 
             }
         });
+
+
+        }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+
+
 
 
 
--- app/src/main/java/com/example/carmen/name_app/ViewScoreActivity.java
+++ app/src/main/java/com/example/carmen/name_app/ViewScoreActivity.java
@@ -1,13 +1,32 @@
 package com.example.carmen.name_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ViewScoreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        int score = getIntent().getIntExtra("score", 0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_score);
+
+        TextView scoreView = findViewById(R.id.scoreView);
+        scoreView.setText(scoreView.getText() + " " + score);
+
+        final Button buttonHome = findViewById(R.id.buttonHome);
+
+        buttonHome.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent(ViewScoreActivity.this, MainActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
