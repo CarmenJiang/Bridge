5e656e669c59ff452ae8bab05ed51184c4f5ab01
h129411 <jorgenschreiner@hotmail.com>
2018-02-02 21:12:28 +0100

    Added testclass for LearningModeQuestionTest Added method to test that correct answer gives one point

--- app/src/androidTest/java/com/example/carmen/name_app/LearningModeQuestionTest.java
+++ app/src/androidTest/java/com/example/carmen/name_app/LearningModeQuestionTest.java
@@ -1,43 +1,63 @@
 package com.example.carmen.name_app;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-
-import org.junit.runner.RunWith;
-
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.filters.LargeTest;
+import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.v4.content.ContextCompat.startActivity;
+import static android.util.Log.*;
+import static android.util.Log.e;
 import static org.junit.Assert.*;
 
-    @RunWith(AndroidJUnit4.class)
-    public class LearningModeQuestionTest {
+/**
+ * Created by jurg on 02/02/2018.
+ */
 
-        @Rule
-        public ActivityTestRule<LearningModeQuestion> mActivityRule;
+@RunWith(AndroidJUnit4.class)
+public class LearningModeQuestionTest {
 
-        public LearningModeQuestionTest() {
-            mActivityRule = new ActivityTestRule(LearningModeQuestion.class);
-        }
+    @Rule
+    public ActivityTestRule<LearningModeQuestion> activityRule
+            = new ActivityTestRule<>(
+            LearningModeQuestion.class,
+            true,     // initialTouchMode
+            false);
 
-        @Test
-        public void correctAnswerGivesPoint() {
+    @Test
+    public void correctAnswerGivesPoint() {
 
-            LearningModeQuestion main = mActivityRule.getActivity();
-            int startPoints = main.score;
-            onView(withId(R.id.enterName)).perform(typeText(main.correctName));
-            onView(withId(R.id.buttonNext)).perform(click());
-            assertEquals(startPoints + 1, main.score);
-        }
+        Intent intent = new Intent();
+        activityRule.launchActivity(intent);
+
+        String correct = activityRule.getActivity().correctName;
+        int startpoints = activityRule.getActivity().score;
+        onView(withId(R.id.enterName)).perform(typeText(correct));
+        onView(withId(R.id.buttonNext)).perform(click());
+        assertEquals(startpoints + 1, activityRule.getActivity().score);
+
     }
+
+    @Test
+    public void wrongAnswerGivesNoPoint() {
+
+        Intent intent = new Intent();
+        activityRule.launchActivity(intent);
+
+        String inCorrect = "wrong";
+        int startpoints = activityRule.getActivity().score;
+        onView(withId(R.id.enterName)).perform(typeText(inCorrect));
+        onView(withId(R.id.buttonNext)).perform(click());
+        assertEquals(startpoints, activityRule.getActivity().score);
+
+    }
+
+}
